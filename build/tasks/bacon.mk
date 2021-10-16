a1b138f309f9d25b31260cea0e955597471b3f2b
xawlw <abdulazizawlw@gmail.com>
2021-10-16 11:42:16 +0200

    vendor: Change MD5 to SHA256
    * Now we generate SHA256 instead of MD5
    
    Change-Id: I464fd2540005599681e8a896d95f35721f61145e
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -30,7 +30,7 @@ bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) $(SHA256) $(EUNOIA_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(EUNOIA_TARGET_PACKAGE).sha256sum
 	echo -e ${CL_BLD}${CL_GRN}"===============================-Package complete-==============================="${CL_GRN}
 	echo -e ${CL_BLD}${CL_RED}"Zip: "${CL_GRN} $(EUNOIA_TARGET_PACKAGE)${CL_RST}
-	echo -e ${CL_BLD}${CL_RED}"MD5: "${CL_GRN}" `cat $(EUNOIA_TARGET_PACKAGE).md5sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_RED}"SHA256: "${CL_GRN}" `cat $(EUNOIA_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_RED}"Size:"${CL_GRN}" `du -sh $(EUNOIA_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_RED}"TimeStamp:"${CL_GRN}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_RED}"Integer Value:"${CL_GRN}" `wc -c $(EUNOIA_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
