cacf92c7a90ac7778e0638183b8535ddfd552131
Luofan Chen <clfbbn@gmail.com>
2023-03-01 23:20:56 +0800

    config: Copy Lineage health & livedisplay permissions
    Change-Id: I12dcb124293cac0ba6f7275ee30498b711c723dd
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -7,7 +7,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 # Permissions for lineage sdk services
 PRODUCT_COPY_FILES += \
-    vendor/eunoia/config/permissions/privapp-permissions-eunoia-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-eunoia-product.xml
+    vendor/eunoia/config/permissions/privapp-permissions-eunoia-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-eunoia-product.xml \
+    vendor/linevall/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml \
+    vendor/linevall/config/permissions/org.lineageos.livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.livedisplay.xml
 
 # Enable one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
